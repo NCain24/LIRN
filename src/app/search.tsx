9d50a39b461eb9380ef511bee311e007e29a2d48
Noah Cain <88724898+NCain24@users.noreply.github.com>
2023-10-01 16:35:12 -0700

    Search functionality

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,25 +1,55 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import users from '../assets/data/users.json';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import UserListItem from '../components/UserListItem';
 import { useLayoutEffect, useState } from 'react';
 import { useNavigation } from 'expo-router';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query profileSearch($term: String) {
+    profileSearch(term: $term) {
+      id
+      image
+      name
+      position
+    }
+  }
+`;
 
 export default function SearchScreen() {
-  const [Search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
   const navigation = useNavigation();
+
+  const { data, loading, error } = useQuery(query, {
+    variables: { term: `%${search}%` },
+  });
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerSearchBarOptions: {
         placeholder: 'Search users',
-        onChangeText: setSearch,
+        onChangeText: (event) => setSearch(event.nativeEvent.text),
       },
     });
   }, [navigation]);
+
+  if (loading && !data?.profileSearch) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Something went wrong.</Text>;
+  }
   return (
     <View style={{ backgroundColor: 'white', flex: 1 }}>
       <FlatList
-        data={users}
+        contentContainerStyle={{ marginTop: 15 }}
+        data={data?.profileSearch}
         renderItem={({ item }) => <UserListItem user={item} />}
       />
     </View>
